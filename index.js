8d3928247f67d451a90c7c6c66ea26cf4d48d9ea
Dhanushsgowda <88840968+Dhanushsgowda@users.noreply.github.com>
2021-12-20 21:07:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// CLicked on submit
+
+var userName = document.querySelector(".login-form-username");
+var userPassword = document.querySelector(".login-form-password");
+let submitBtn = document.getElementById("submit");
+let form = $("#login-form");
+
+
+form.submit(function(e){
+    e.preventDefault();
+
+    const user = userName.value;
+    const pwd = userPassword.value;
+    var mObj = {
+      username: user,
+      password: pwd,
+    };
+
+    // if username and password are same
+
+    if (user === pwd && user !== "" && pwd !== "") {
+        alert("Login‌ ‌Successful");
+        localStorage.setItem("loginStatus", JSON.stringify(mObj));
+        location.assign("./orders.html");
+    }
+
+    else {
+        alert("Please‌‌ enter‌ ‌valid‌ ‌credentials!");
+    }
+})
+
+document.getElementById("sign-out").style.display = "none";
